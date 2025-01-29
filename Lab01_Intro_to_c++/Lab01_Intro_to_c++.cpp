f99eb5ac3b34adee94f606fd0208e2c48a277ba4
bentatam <146729919+bentatam@users.noreply.github.com>
2025-01-29 13:55:12 +0000

    Lab 1

--- Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
+++ Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
@@ -1,1 +1,120 @@
+#include <iostream>
 
+
+float pi = 3.145927f;
+
+
+//power function
+float power(const float x, const int y)
+{
+	float xPowerY = 1.0f;
+	for (unsigned int i = 0; i < y; i++)
+	{
+		xPowerY *= x;
+	}
+
+	return xPowerY;
+}
+
+//degrees to Radians Function
+float radians(const float angle)
+{
+	return angle * pi / 180.0f;
+}
+
+int main()
+{
+	std::cout << "Hello World\n" << std::endl;
+
+	//degrees to radians conversion
+
+	float angleInDegrees = 45.00f;
+
+
+	float angleInRadians = angleInDegrees * pi / 180.0f;
+
+	//output result
+
+	std::cout << angleInDegrees << " degrees is equal to "
+		<< angleInRadians << " radians." << std::endl;
+
+	//Determine wether it is acute or obtuse
+	std::cout << "\nIf statements\n---------------" << std::endl;
+	std::cout << angleInDegrees << " degrees is an ";
+
+	if (angleInDegrees < 90)
+		std::cout << "acute angle." << std::endl;
+
+	else
+		std::cout << " obtuse angle." << std::endl;
+
+
+	//For Loops
+	
+	std::cout << "\nFor loop\n-----" << std::endl;
+	int powerOfTwo = 1;
+
+	for (unsigned int i = 0; i < 10; i++)
+	{
+		powerOfTwo *= 2;
+		std::cout << "2^" << i + 1 << " = " << powerOfTwo << std::endl;
+	}
+
+	//Count the number of steps the collatz sequence takes to reach 1
+	int n, step, nsave;
+	n = 10;
+	step = 0;
+	nsave = n;
+	std::cout << "\nWhile loop\n---------" << n;
+
+	while(n > 1)
+	{
+		if (n % 2 == 0)
+		{
+			n /= 2;
+		}
+
+		else
+		{
+			n = 3 * n + 1;
+		}
+
+		step++;
+		std::cout << " -> " << n;
+	}
+
+	std::cout << "\n\nThe Collatz sequence starting at " << nsave
+		<< " took " << step << " steps to reach 1." << std::endl;
+
+	//Arrays
+	unsigned int myArray[] =
+	{
+		3, 1, 4, 1, 5, 9, 2, 6, 5
+	};
+
+	std::cout << "\nArrays\n-----" << std::endl;
+
+	for (unsigned int i = 0; i < 9; i++)
+	{
+		std::cout << "myArray[" << i << "] = " << myArray[i] << std::endl;
+	}
+
+	std::cout << "\nSize of myArray = " << sizeof(myArray) << std::endl;
+
+	std::cout << "\nNumber of Elements = " << sizeof(myArray) / sizeof(unsigned int) << std::endl;
+
+
+	//power
+
+	float twoPowerTen = power(2.0f, 10);
+
+	std::cout << "\nFunctions\n-------------" << std::endl;
+	std::cout << "2^10 = " << twoPowerTen << std::endl;
+
+	//Call radians function
+	float angle = angleInDegrees;
+	angle = radians(angle);
+
+	std::cout << "\n" << angleInDegrees << " degrees is equal to " << angle << " radians." << std::endl;
+	return 0;
+}
